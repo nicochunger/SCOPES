0da982a18891bbf86421f098495b1870af3b867c
Nicolas Unger <nicolas.unger@unige.ch>
2024-08-06 11:27:25 +0200

    Added matplotlib as requirement

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         "numpy",
         "pandas",
+        "matplotlib",
         "astropy",
         "astroplan",
         "tqdm",
@@ -36,8 +37,6 @@ setup(
         "Topic :: Scientific/Engineering :: Astronomy",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
